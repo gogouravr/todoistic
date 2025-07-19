e56e2cc54cef7a923d3e75cf2a7d9fd4ff05eb9d
gogouravr <gogouravr@gmail.com>
2025-07-19 22:09:18 +0530

    Setup tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
+import Home from './components/Home'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello!</h1>
-    </>
+    <Home />
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+
+const Home = () => {
+    return <h1 class="bg-sky-50" >Home Page</h1>
+}
+
+
+export default Home;
